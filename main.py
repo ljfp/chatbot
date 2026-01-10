2f0a37a9c14323ed3abdab18b6be793a752e1825
Lautaro Javier Fernandez Pricco <ljfp@ljfp.xyz>
2026-01-10 12:17:56 +0100

    Print token count.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from agent!")
+import os
+from types import resolve_bases
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("Gemini api key is missing")
+
+    client = genai.Client(api_key=api_key)
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model = "gemini-2.5-flash"
+    response = client.models.generate_content(model=model, contents=prompt)
+    if not response.usage_metadata:
+        raise RuntimeError("Failed API request.")
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
