3659a51f35f382489c71133dc81dbffc13ec6783
Lautaro Javier Fernandez Pricco <ljfp@ljfp.xyz>
2026-01-11 16:43:10 +0100

    Add CLI to interact with the agent

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import argparse
 import os
-from types import resolve_bases
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -9,15 +10,24 @@ def main():
     if not api_key:
         raise RuntimeError("Gemini api key is missing")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    prompt = args.user_prompt
     model = "gemini-2.5-flash"
-    response = client.models.generate_content(model=model, contents=prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(
+        model=model, contents=messages
+    )
     if not response.usage_metadata:
         raise RuntimeError("Failed API request.")
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response tokens: {response.usage_metadata.prompt_token_count}")
+    if args.verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response: {response.text}")
 
 if __name__ == "__main__":
